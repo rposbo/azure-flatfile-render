ed36d58ec5c0964df3f8ae7aabc44f9f3f30c25a
Robin Osborne <robin.osborne@gmail.com>
2013-11-11 22:09:57 +0000

    init innit

---
+++ Render/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Render.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index(string id)
+        {
+            var model = ProductDataService.GetById(id);
+            return View(model);
+        }
+    }
+
+    public static class ProductDataService
+    {
+        private const string DemoProductData = @"{{'name':'myproduct', 'title':'product of mine', 'price': '£25', 'id' :{0}}}";
+
+        public static dynamic GetById(string id)
+        {
+            var result = Json.Decode(string.Format(DemoProductData, id));
+            return result;
+        }
+    }
+}
